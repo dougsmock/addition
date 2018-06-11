e327956d5ec71afb276bf519e503f21751989eb4
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-11 13:45:46 -0400

    Update addition.rb

--- addition.rb
+++ addition.rb
@@ -1,1 +1,30 @@
 
+  print "Let's add two numbers. Give me one: "
+  a = gets.chomp.to_i
+  print "Give me the other: "
+  b = gets.chomp.to_i
+  answer = a + b
+  print "#{answer}\n\n"
+
+  print "Let's subtract two numbers. Give me one: "
+  a = gets.chomp.to_i
+  print "Give me the other: "
+  b = gets.chomp.to_i
+  answer = a - b
+  print "#{answer}\n\n"
+
+
+  print "Let's multiply two numbers. Give me one: "
+  a = gets.chomp.to_i
+  print "Give me the other: "
+  b = gets.chomp.to_i
+  answer = a * b
+  print "#{answer}\n\n"
+
+  print "Let's divide two numbers. Give me one: "
+  a = gets.chomp.to_f
+  print "Give me the other: "
+  b = gets.chomp.to_f
+  answer = a / b
+  print "#{answer}\n\n"
+
